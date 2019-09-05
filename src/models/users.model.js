95613269e6432d39fe6a38b962bf4bcc4e7082d0
Matthew Caloger <mattcaloger@gmail.com>
2019-09-05 12:42:46 -0400

    Resolves issue #2
    Added [firstName, lastName, department, businessTitle, and enabled] fields. Added 'required: true'  to [email, password, firstName, and lastName, and enabled] fields.

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,17 +2,22 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-  const users = new mongooseClient.Schema({
-  
-    email: {type: String, unique: true, lowercase: true},
-    password: { type: String },
-  
-  
-  }, {
-    timestamps: true
-  });
+module.exports = function(app) {
+  const mongooseClient = app.get("mongooseClient");
+  const users = new mongooseClient.Schema(
+    {
+      email: { type: String, unique: true, lowercase: true, required: true },
+      password: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      department: { type: String },
+      businessTitle: { type: String },
+      enabled: { type: boolean, default: true, required: true }
+    },
+    {
+      timestamps: true
+    }
+  );
 
-  return mongooseClient.model('users', users);
+  return mongooseClient.model("users", users);
 };
